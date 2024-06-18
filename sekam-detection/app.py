c697711e4fc0194f45d18f6e129cd243fc633e61
rifqifazzam <rifqifauzan709@gmail.com>
2024-06-18 20:15:55 +0700

    chore: add about page to the web application

--- sekam-detection/app.py
+++ sekam-detection/app.py
@@ -42,6 +42,10 @@ def process_image(file):
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
